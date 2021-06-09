a2222bfa75644ac5b9e0b3651cd781b81f631785
N-Ogura666 <ogura@griffin-net.co.jp>
2021-06-09 18:33:36 +0900

    （村井さん）名前項目・投稿時刻の追加

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,43 @@
-function addTodoPicture() {
-    navigator.camera.getPicture(addTodo, function() {
-        alert("Failed to get camera.");
-    }, {
-        quality : 50,
-        destinationType : Camera.DestinationType.DATA_URL,
-        targetWidth : 100,
-        targetHeight : 100
-    });
-}
+// function addTodoPicture() {
+//     navigator.camera.getPicture(addTodo, function() {
+//         alert("Failed to get camera.");
+//     }, {
+//         quality : 50,
+//         destinationType : Camera.DestinationType.DATA_URL,
+//         targetWidth : 100,
+//         targetHeight : 100
+//     });
+// }
 function addTodo(camera_url) {
     var title = $("#todo-title").val();
+    //名前の取得
+    var name = $("#todo-name").val();
+    //現在日時の取得
+    var date = new Date();
+      var year = date.getFullYear();
+      var month = (date.getMonth()+1)
+      var day = date.getDate();
+      var hour = date.getHours();
+      var minutes = date.getMinutes();
+      //時刻表示の桁数を２桁に揃える
+      if (hour < 10) {
+      hour = "0" + hour;
+      }
+      if (minutes < 10) {
+      minutes = "0" + minutes;
+      }
     var body = $("#todo-body").val();
     var img_tag = "";
     if (camera_url) {
         img_tag = "<img src='data:image/jpeg;base64," + camera_url + "'>";
     }
     $.mobile.changePage($("#list-page"));
-    $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
+    $("#todo-list").append(
+      "<li>" + img_tag +
+      "<h3>" + title + "</h3><p>" +
+      name + " " + 
+      year + "/" + month + "/" + day + " " + hour + ":" + minutes + "<p>" +
+      body + "</p></li>"
+      )
     $("#todo-list").listview('refresh');
 };
